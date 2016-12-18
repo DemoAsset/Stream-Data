4dcab15f3a0cb0dbe8c4fc0bdf33199acf163fb0
Priyeshmshr <priyesh.mshr@gmail.com>
2016-12-19 01:16:27 +0530

    Updated data model

--- app/viewmodels/datamodel.js
+++ app/viewmodels/datamodel.js
@@ -5,7 +5,6 @@ function account(accountNumber, name) {
 	var self = this;
 	self.accountNumber = accountNumber;
 	self.name = name;
-	
 	self.getAccountString = ko.computed(function(){
 		var acc = self.accountNumber.toString();
 		var dash = acc.concat(" - ");
@@ -15,12 +14,19 @@ function account(accountNumber, name) {
 }
 
 function merchantCategoryCode(mcc, storeName) {
-	this.mcc = mcc;
-	this.storeName = storeName;
+	var self = this;
+	self.mcc = mcc;
+	self.storeName = storeName;
+	self.getMCCString = ko.computed(function(){
+		var mcc = self.mcc.toString();
+		var dash = mcc.concat(" - ");
+		var res = dash.concat(self.storeName);
+		return res;
+		});
 }
+
 function appViewModel(){
 	var self = this;
-	self.amount = ko.observable("200");
 	self.accountsList = ko.observableArray(['5989919999999990 - Shea Parker', 'Germany', 'Spain']);
 	self.accounts = ko.observableArray([
 					 new account(5989919999999990, "Shea Parker"),
@@ -28,6 +34,40 @@ function appViewModel(){
 					 new account(9846511110000919, "Barbara Stockton"),
 					 new account(9846511116655442, "Barbara Stockton"),
 					 new account(4659984848, "Barbara Stockton")
-					]);
+					]),
+   
+   self.selectedAccount =ko.observable("");
+   self.merchantCode= ko.observableArray([
+   	          new merchantCategoryCode(5198, "Home Improvement"),
+   	          new merchantCategoryCode(5200, "Home Improvement"),
+   	          new merchantCategoryCode(5211, "Home Improvement"),
+   	          new merchantCategoryCode(5712, "Home Improvement"),
+   	          new merchantCategoryCode(1520, "Home Improvement"),
+   	          new merchantCategoryCode(5541, "Gas Station"),
+   	          new merchantCategoryCode(5542, "Gas Station")
+   ]);
+   self.buttonText = ko.observable("Start");
+   self.tempList= ko.observableArray(["One","Two"]);
+    self.streamData= function(){
+   	     	var temp = self.buttonText();
+   	     	var myVar = "";
+   	     	if(temp==="Start")
+   	     	temp="Stop";
+   	     	else{
+   	     	temp="Start";
+   	     	}
+   	     	self.buttonText(temp);
+   	     	myVar = setInterval(function(){
+   	     			tempList.push("Three");
+   	     			},2000);
+   	     	/*while(temp==="Stop"){
+   	     	}*/
+   };
+   self.selectedMCC = ko.observable();
+   self.minTransAmount = ko.observable(20);
+   self.maxTransAmount = ko.observable(1000);
+   self.randomAccounts = ko.observable(false);
+   self.randomMCC = ko.observable(false);
+   self.sendTransactionStream = ko.observable(false);
 }
-ko.applyBindings(new appViewModel());
+ko.applyBindings(appViewModel);
