4f67fb947a7c961820eed136eff5ff611dc39a2f
Priyeshmshr <priyesh.mshr@gmail.com>
2016-12-20 00:02:11 +0530

    Data model complete.

--- app/viewmodels/datamodel.js
+++ app/viewmodels/datamodel.js
@@ -1,6 +1,9 @@
 /**
  * @author mishp3
  */
+
+var sendData,worker,tempList,transactionList, url = 'http://10.61.9.168:7003/stream/CreditCardTransactionStream';
+
 function account(accountNumber, name) {
 	var self = this;
 	self.accountNumber = accountNumber;
@@ -25,17 +28,49 @@ function merchantCategoryCode(mcc, storeName) {
 		});
 }
 
+function transactionData(accountNumber, amount, mcc, transID){
+	var self = this;
+	self.CCT_ACCTNBR = accountNumber;
+	self.CCT_TRANAMOUNT = amount;
+	self.CCT_MERCHANTCATEGORYCODE = mcc;
+	self.CCT_TRANSEQ = transID;
+	//self.dateTimeStamp = dateTimeStamp;
+}
+
+function sendAjaxRequest(data){
+	$.ajax({
+		type : 'POST',
+		url : url,
+		dataType : 'json',
+		contentType : "application/json",
+		crossDomain :true,
+		headers : {
+			"Authorization" : "Basic " + btoa("MarketingAdministratorFS" + ":" + "install"),
+		},
+		data : JSON.stringify(data),
+		success : function() {
+			//var trans = JSON.parse(data);
+			//transactionList.push(new transactionData(trans.selectedAccount,trans.transAmount,trans.selectedMCC,trans.transID));
+			transactionList.push(data);
+		},
+		error: function(e){
+		    console.log(e);	
+		    //var trans = JSON.parse(data);
+		    //transactionList.push(new transactionData(trans.selectedAccount,trans.transAmount,trans.selectedMCC,trans.transID));
+		    transactionList.push(data);
+		}
+	}); 
+}
 function appViewModel(){
 	var self = this;
-	self.accountsList = ko.observableArray(['5989919999999990 - Shea Parker', 'Germany', 'Spain']);
+	transactionList = ko.observableArray();
 	self.accounts = ko.observableArray([
 					 new account(5989919999999990, "Shea Parker"),
 					 new account(9999999975, "Shea Parker"),
 					 new account(9846511110000919, "Barbara Stockton"),
 					 new account(9846511116655442, "Barbara Stockton"),
 					 new account(4659984848, "Barbara Stockton")
-					]),
-   
+					]);
    self.selectedAccount =ko.observable("");
    self.merchantCode= ko.observableArray([
    	          new merchantCategoryCode(5198, "Home Improvement"),
@@ -46,28 +81,96 @@ function appViewModel(){
    	          new merchantCategoryCode(5541, "Gas Station"),
    	          new merchantCategoryCode(5542, "Gas Station")
    ]);
+   self.randomAccountsList = [
+                  new account(9999999998, "Skyla Anderson"),
+                  new account(9999999992, "Talia Green"),
+                  new account(9999999993, "Talia Green"),
+                  new account(9999999990, "Skyla Anderson")     
+   ];
+   self.randomMerchantCode = [
+                  new merchantCategoryCode(1712, "Random"),
+                  new merchantCategoryCode(5073, "Random"),
+                  new merchantCategoryCode(1110, "Random"),
+                  new merchantCategoryCode(1111, "Random")
+   ];
    self.buttonText = ko.observable("Start");
-   self.tempList= ko.observableArray(["One","Two"]);
-    self.streamData= function(){
+   
+   /**
+    *   CallBack function to start and stop transaction stream 
+    */
+   self.streamData= function(){
    	     	var temp = self.buttonText();
-   	     	var myVar = "";
-   	     	if(temp==="Start")
+   	     	if(temp==="Start"){
    	     	temp="Stop";
+   	     	sendStream();
+   	     	//startWorker();
+   	     	}
    	     	else{
    	     	temp="Start";
+   	     	clearInterval(sendData);
+   	     	//stopWorker();
    	     	}
    	     	self.buttonText(temp);
-   	     	myVar = setInterval(function(){
+   	     	/*myVar = setInterval(function(){
    	     			tempList.push("Three");
-   	     			},2000);
-   	     	/*while(temp==="Stop"){
-   	     	}*/
+   	     			},2000);*/
+   	     	
    };
+   
+   /**
+    *  Helper function to create JSON object and send it to the server. 
+    */
+   self.sendStream = function(){
+   
+   sendData = setInterval(function(){
+   	     			//tempList.push("Three");
+   	     			var data = createTransData();
+   	     			sendAjaxRequest(data);
+   	     			},2000);
+   };
+   
+   
    self.selectedMCC = ko.observable();
    self.minTransAmount = ko.observable(20);
    self.maxTransAmount = ko.observable(1000);
    self.randomAccounts = ko.observable(false);
    self.randomMCC = ko.observable(false);
-   self.sendTransactionStream = ko.observable(false);
+   
+   
+    /**
+     * Creates a JSON representation for this view Model. 
+     */
+	function createTransData() {
+		var transAmount = Math.floor(Math.random() * (self.maxTransAmount() - self.minTransAmount() + 1) + self.minTransAmount());
+		var ranAccIndex,ranMCCIndex;
+
+		if (self.randomAccounts())
+			ranAccIndex = Math.floor(Math.random() * (randomAccountsList.length + 1));
+		if (self.randomMCC())
+			ranMCCIndex = Math.floor(Math.random() * (randomMerchantCode.length + 1));
+
+        console.log("ranAccIndex: " + ranAccIndex + " randomAccountsList: " + randomAccountsList.length);
+		var jsonObj = new Object();
+		if (ranAccIndex < randomAccountsList.length) {                         //Stores Account Number in JSON
+			var obj = randomAccountsList[ranAccIndex];
+			jsonObj.selectedAccount = obj.accountNumber;
+		}
+		else
+			jsonObj.selectedAccount = self.selectedAccount();
+
+		if (ranMCCIndex < randomMerchantCode.length)                          //Stores Merchant Category Code in JSON
+			jsonObj.selectedMCC = randomMerchantCode[ranMCCIndex].mcc;
+		else
+			jsonObj.selectedMCC = self.selectedMCC();
+
+		jsonObj.transAmount = transAmount;                                    //Stores Transaction Amount in JSON
+		
+		//Update to generate random tansaction id
+		jsonObj.transID = 100;												  //Stores Unique Transaction ID in JSON 
+		
+		
+		//return JSON.stringify(jsonObj);
+		return new transactionData(jsonObj.selectedAccount,jsonObj.transAmount,jsonObj.selectedMCC, jsonObj.transID);
+	}; 
 }
 ko.applyBindings(appViewModel);
